ab24a0180edf629ba382a9472a9450d613b4d050
nbermudez <nestor.bermudez@unitec.edu>
2013-05-29 17:20:48 -0600

    error in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Regionsps::Application.config.secret_token = '834ff75c26ffc066b8ccf94ca87ff296d0240a92687a04163c7f11f5e887ab05d03f54a1ae5479c41c26197f4ca9caed3f3f2f8429976d3342f9a1122280c923'
+Regionsps::Application.config.secret_token = 'ebaa5f1fbc5b02e53d2e45ef647e4fd3149c101f1b00d9459b69f57bee5b44d5621c836710466e2d0dc4a3c15c4f22508da9c458fa061b7ebe16aa3835de543a'
