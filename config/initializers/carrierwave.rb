ce35381243fc8e13153db542ab001d9984bfcafd
nbermudez <nestor.bermudez@unitec.edu>
2013-07-31 15:18:40 -0600

    Changing to S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,4 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
+
     # Configuration for Amazon S3 should be made available through an Environment variable.
     # For local installations, export the env variable through the shell OR
     # if using Passenger, set an Apache environment variable.
@@ -14,24 +13,17 @@ CarrierWave.configure do |config|
     # S3_BUCKET_NAME: regionsps_bucket/public
     # S3_ASSET_URL: regionsps_bucket.s3-website-us-east-1.amazonaws.com
     # Configuration for Amazon S3
-    :provider              => 'AWS',
-    :aws_access_key_id     => ENV['S3_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET'],
-    :region                => ENV['S3_REGION']
+
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV['S3_KEY'],                        # required
+    :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
+    :region                 => ENV['S3_REGION'],                  # optional, defaults to 'us-east-1'
+    :host                   => 'regionsps_bucket.s3-website-us-east-1.amazonaws.com',             # optional, defaults to nil
+    :endpoint               => 'https://regionsps_bucket.s3-website-us-east-1.amazonaws.com:8080' # optional, defaults to nil
   }
- 
-  # For testing, upload files to local `tmp` folder.
-  if Rails.env.test? || Rails.env.cucumber?
-    config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
-  else
-    config.storage = :fog
-  end
- 
-  config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
- 
-  config.fog_directory    = ENV['S3_BUCKET_NAME']
-  config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
-  config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+  config.fog_directory  = ENV['S3_BUCKET_NAME']                    # required
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
