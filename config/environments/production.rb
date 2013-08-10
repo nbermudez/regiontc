2f747b9a660ca9ac7106fbb0a0cb20291e066064
nbermudez <nestor.bermudez@unitec.edu>
2013-08-09 19:12:40 -0600

    More changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Regionsps::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            'nestor.bermudez@unitec.edu',
-    password:             'potato97783870',
+    user_name:            'no-reply@regionsps.org',
+    password:             'No-responder',
     authentication:       'plain',
     enable_starttls_auto: true  
   }
