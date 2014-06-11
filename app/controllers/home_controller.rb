5e5b6b9db14897eec7f93af7bee88a2ee288feeb
Alex Fernandez <a.fernandez@mayanx.com>
2014-06-11 11:51:11 -0600

    Articles nessesary files

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ActionController::Base
 	def index
 		@events = Event.where(:is_public => true).where("celebrated_at > (?)", Date.yesterday)
+    @articles = Article.where(:published => true, :active=>true).order('created_at desc').limit(5)
 	end
 end
-
-#Cindy Aguirre: 96047455
