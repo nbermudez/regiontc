7b8e6bf039a7077506ae0a666919e369d07d2559
Alex Fernandez <a.fernandez@mayanx.com>
2014-06-12 15:57:01 -0600

    test created by the generate function of rails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,16 @@ Regionsps::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            'mhotivo@gmail.com',
+    password:             'abc098765',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
